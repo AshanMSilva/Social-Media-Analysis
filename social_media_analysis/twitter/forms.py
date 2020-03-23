bce4ebf3cd48e0078b8c59fe24a986b6bed1bcb3
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-03-23 12:58:34 +0530

    comments error fixed

---
+++ social_media_analysis/twitter/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class screenNameForm(FlaskForm):
+    name = TextAreaField('Screen_Name', validators=[DataRequired()])
+    submit = SubmitField('Search')
