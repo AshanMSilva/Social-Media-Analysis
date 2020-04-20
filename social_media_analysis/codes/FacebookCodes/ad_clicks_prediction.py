b9a46ab166768b019b1fdcc1d1cc0a35fa237290
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2020-04-20 09:05:47 +0530

    ad clicks

--- social_media_analysis/codes/FacebookCodes/ad_clicks_prediction.py
+++ social_media_analysis/codes/FacebookCodes/ad_clicks_prediction.py
@@ -13,16 +13,11 @@ class AdPrediction():
 
 class AdImpressionPrediction():
     def predict(self,input_data):
-        # filename = 'social_media_analysis\codes\FacebookCodes\\facebooklik\.csv'
-        # f = 
         filename = 'social_media_analysis\codes\FacebookCodes\\fbAdsImpreModel.sav'
         loaded_model = pickle.load(open(filename, 'rb'))
-        # f.seek(0)\
-        result = loaded_model.predict(input_data)
+        result = loaded_model.predict(input_data) 
         return result
-
-# final={'mon':[mon] ,'tue':[tue] ,'wed':[wed] ,'thu':[thu] ,'fri':[fri] ,'sat':[sat] ,'sun':[sun] ,'male':[male] ,'female':[female] ,'all':[all] ,'TextWordCount':[len(txt.split())] ,'startAge':[startAge] ,'endAge':[endAge],'AdImpressions':[impre_output],'AdSpends':[adSpends] }
-#  prediction_impre   
+ 
 class BestSolutions():
     def getBestGender(self,input_data,model):
         input_data['male']=1
@@ -44,15 +39,24 @@ class BestSolutions():
         input_data['all']=1
         final_features=pd.DataFrame.from_dict(input_data)
         all_result= model.predict(final_features)
-        return [int(female_result),int(male_result),int(all_result)]
+        return [int((male_result**2)**0.5),int((female_result**2)**0.5),int((all_result**2)**0.5)]
 
-    def getBestSpend(self,input_data,model):
+    def getBestSpend(self,input_data,clicks_model,impre_model):
         spend_results=[]
         first=int(input_data['AdSpends'][0])
         for money in range(first,first+301,10):
             input_data['AdSpends']=money
             final_features=pd.DataFrame.from_dict(input_data)
-            spend_results.append([money,int(model.predict(final_features)[0])])
+            impressions=int((impre_model.predict(final_features)[0]**2)**0.5)
+
+            clicks_input_data=input_data
+            del clicks_input_data['AdSpends']
+            clicks_input_data['AdImpressions']=impressions
+            clicks_input_data['AdSpends']=money
+            clicks_final_features= pd.DataFrame.from_dict(clicks_input_data)
+            clicks=int((clicks_model.predict(clicks_final_features)[0]**2)**0.5)
+            del clicks_input_data['AdImpressions']
+            spend_results.append([money,clicks,impressions])
         return spend_results
 
     def getBestWeekDay(self,input_data,model):
@@ -64,7 +68,7 @@ class BestSolutions():
         for day in days:
             input_data[day]=1
             final_features=pd.DataFrame.from_dict(input_data)
-            week_day_results[day]=int(model.predict(final_features)[0])
+            week_day_results[day]=int((model.predict(final_features)[0]**2)**0.5)
             input_data[day]=0
         return week_day_results
             
