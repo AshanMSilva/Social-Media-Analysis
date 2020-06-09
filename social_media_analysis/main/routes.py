acb0ae419a4b841e994423e021a9c39ee5568e67
VimuthR <vimuth.17@cse.mrt.ac.lk>
2020-06-09 12:55:16 +0530

    Add files via upload

--- social_media_analysis/main/routes.py
+++ social_media_analysis/main/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, Blueprint, redirect, url_for
+from flask import render_template, request, Blueprint, redirect, url_for,flash
 from social_media_analysis.models import Post
 from social_media_analysis.twitter.forms import NameForm, BotForm,HashtagForm, TweetForm
+from social_media_analysis.youtube.forms import ChannelDetailForm,SentimentAnalysisForm,ViewPredictionForm,ViralVideoForm
 from social_media_analysis.posts.forms import PostForm
 from social_media_analysis.users.forms import RegistrationForm, LoginForm
 
@@ -10,29 +11,29 @@ main = Blueprint('main', __name__)
 @main.route("/", methods=['GET', 'POST'])
 @main.route("/home", methods=['GET', 'POST'])
 def home():
-	try:
-		loginmodalshow='close'
-		loginform = LoginForm()
-		if(loginform.validate_on_submit()==False and loginform.login.data):
-			loginmodalshow='loginformmodal'
-		if loginform.validate_on_submit() and loginform.login.data:
-			remember=loginform.remember.data
-			email=loginform.email.data
-			password=loginform.password.data
-			return redirect(url_for('users.login', remember=remember, email=email, password=password))
-		modalshow='close'
-		registerform = RegistrationForm()
-		if(registerform.validate_on_submit()==False and registerform.signup.data):
-			modalshow='registerformmodal'
-		if registerform.validate_on_submit() and registerform.signup.data:
-			username=registerform.username.data
-			email=registerform.email.data
-			password=registerform.password.data
-			return redirect(url_for('users.register', username=username, email=email, password=password))
-		return render_template('home.html', registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow)
-	except:
-		flash('Something went Wrong. Please check weather enterd details are correct', 'warning')
-		return redirect(url_for('main.home'))
+    try:
+        loginmodalshow='close'
+        loginform = LoginForm()
+        if(loginform.validate_on_submit()==False and loginform.login.data):
+            loginmodalshow='loginformmodal'
+        if loginform.validate_on_submit() and loginform.login.data:
+            remember=loginform.remember.data
+            email=loginform.email.data
+            password=loginform.password.data
+            return redirect(url_for('users.login', remember=remember, email=email, password=password))
+        modalshow='close'
+        registerform = RegistrationForm()
+        if(registerform.validate_on_submit()==False and registerform.signup.data):
+            modalshow='registerformmodal'
+        if registerform.validate_on_submit() and registerform.signup.data:
+            username=registerform.username.data
+            email=registerform.email.data
+            password=registerform.password.data
+            return redirect(url_for('users.register', username=username, email=email, password=password))
+        return render_template('home.html', registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow)
+    except:
+        flash('Something went Wrong. Please check weather enterd details are correct', 'warning')
+        return redirect(url_for('main.home'))
 
 @main.route("/about", methods=['GET', 'POST'])
 def about():
@@ -40,97 +41,152 @@ def about():
 
 @main.route("/twitter", methods=['GET', 'POST'])
 def twitter():
-	try:
-		loginmodalshow='close'
-		loginform = LoginForm()
-		if(loginform.validate_on_submit()==False and loginform.login.data):
-			loginmodalshow='loginformmodal'
-		if loginform.validate_on_submit() and loginform.login.data:
-			remember=loginform.remember.data
-			email=loginform.email.data
-			password=loginform.password.data
-			return redirect(url_for('users.login', remember=remember, email=email, password=password))
-		modalshow='close'
-		registerform = RegistrationForm()
-		if(registerform.validate_on_submit()==False and registerform.signup.data):
-			modalshow='registerformmodal'
-		if registerform.validate_on_submit() and registerform.signup.data:
-			username=registerform.username.data
-			email=registerform.email.data
-			password=registerform.password.data
-			return redirect(url_for('users.register', username=username, email=email, password=password))
-		botform = BotForm()
-		if botform.validate_on_submit() and botform.submit.data:
-			bot_account_name = botform.name.data
-			return redirect(url_for('twitter.bot_account_detection', name=bot_account_name))
+    try:
+        loginmodalshow='close'
+        loginform = LoginForm()
+        if(loginform.validate_on_submit()==False and loginform.login.data):
+            loginmodalshow='loginformmodal'
+        if loginform.validate_on_submit() and loginform.login.data:
+            remember=loginform.remember.data
+            email=loginform.email.data
+            password=loginform.password.data
+            return redirect(url_for('users.login', remember=remember, email=email, password=password))
+        modalshow='close'
+        registerform = RegistrationForm()
+        if(registerform.validate_on_submit()==False and registerform.signup.data):
+            modalshow='registerformmodal'
+        if registerform.validate_on_submit() and registerform.signup.data:
+            username=registerform.username.data
+            email=registerform.email.data
+            password=registerform.password.data
+            return redirect(url_for('users.register', username=username, email=email, password=password))
+		
+        botform = BotForm()
+        botmodal='close'
+        if(botform.validate_on_submit()==False and botform.submit.data):
+            botmodal='botdetectionmodel'
+        if botform.validate_on_submit() and botform.submit.data:
+            bot_account_name = botform.name.data
+            return redirect(url_for('twitter.bot_account_detection', name=bot_account_name))
 
-		hashtagform = HashtagForm()
-		if hashtagform.validate_on_submit() and hashtagform.hashtagsubmit.data:
-			hashtag = hashtagform.hashtag.data
-			return redirect(url_for('twitter.hashtag_tweets', hashtag=hashtag))
+        hashtagform = HashtagForm()
+        hashtagmodal='close'
+        if(hashtagform.validate_on_submit()==False and hashtagform.hashtagsubmit.data):
+            hashtagmodal='hashtagmodel'
+        if hashtagform.validate_on_submit() and hashtagform.hashtagsubmit.data:
+            hashtag = hashtagform.hashtag.data
+            return redirect(url_for('twitter.hashtag_tweets', hashtag=hashtag))
 
-		screennameform = NameForm()
-		if screennameform.validate_on_submit() and screennameform.search.data:
-			screen_name = screennameform.name.data
-			return redirect(url_for('twitter.user_details', name=screen_name))
+        screennamemodal='close'
+        screennameform = NameForm()
+        if(screennameform.validate_on_submit()==False and screennameform.search.data):
+            screennamemodal='searchforaccount'
+        if screennameform.validate_on_submit() and screennameform.search.data:
+            screen_name = screennameform.name.data
+            return redirect(url_for('twitter.user_details', name=screen_name))
+        
+        predtweetmodal='close'
+        tweetform = TweetForm()
+        if(tweetform.validate_on_submit()==False and tweetform.likespredict.data):
+            predtweetmodal='likespredmodal'
+        if tweetform.validate_on_submit()and tweetform.likespredict.data:
+            screen_name = tweetform.name.data
+            tweet = tweetform.tweet.data
+            time = tweetform.time.data
+            return redirect(url_for('twitter.user_tweets', name=screen_name, tweet=tweet, time=time))
 
-		tweetform = TweetForm()
-		if tweetform.validate_on_submit() and tweetform.likespredict.data:
-			screen_name = tweetform.name.data
-			tweet = tweetform.tweet.data
-			return redirect(url_for('twitter.user_tweets', name=screen_name, tweet=tweet))
-
-		return render_template('twitter.html', title='Twitter', tweetform=tweetform, screennameform=screennameform, botform=botform, hashtagform=hashtagform, registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow)
-	except:
-		flash('Something went Wrong. Please check weather enterd details are correct', 'warning')
-		return redirect(url_for('main.home'))
+        return render_template('twitter.html', title='Twitter', tweetform=tweetform, screennameform=screennameform, botform=botform, hashtagform=hashtagform, registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow, predtweetmodal=predtweetmodal, screennamemodal=screennamemodal, botmodal=botmodal, hashtagmodal=hashtagmodal)
+    except:
+        flash('Something went Wrong. Please check weather enterd details are correct', 'warning')
+        return redirect(url_for('main.home'))
 @main.route("/facebook", methods=['GET', 'POST'])
 def facebook():
-	loginmodalshow='close'
-	loginform = LoginForm()
-	if(loginform.validate_on_submit()==False and loginform.login.data):
-		loginmodalshow='loginformmodal'
-	if loginform.validate_on_submit() and loginform.login.data:
-		remember=loginform.remember.data
-		email=loginform.email.data
-		password=loginform.password.data
-		return redirect(url_for('users.login', remember=remember, email=email, password=password))
-	modalshow='close'
-	registerform = RegistrationForm()
-	if(registerform.validate_on_submit()==False and registerform.signup.data):
-		modalshow='registerformmodal'
-	if registerform.validate_on_submit() and registerform.signup.data:
-		username=registerform.username.data
-		email=registerform.email.data
-		password=registerform.password.data
-		return redirect(url_for('users.register', username=username, email=email, password=password))
-	return render_template('facebook.html', title='Facebook', registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow)
+    loginmodalshow='close'
+    loginform = LoginForm()
+    if(loginform.validate_on_submit()==False and loginform.login.data):
+        loginmodalshow='loginformmodal'
+    if loginform.validate_on_submit() and loginform.login.data:
+        remember=loginform.remember.data
+        email=loginform.email.data
+        password=loginform.password.data
+        return redirect(url_for('users.login', remember=remember, email=email, password=password))
+    modalshow='close'
+    registerform = RegistrationForm()
+    if(registerform.validate_on_submit()==False and registerform.signup.data):
+        modalshow='registerformmodal'
+    if registerform.validate_on_submit() and registerform.signup.data:
+        username=registerform.username.data
+        email=registerform.email.data
+        password=registerform.password.data
+        return redirect(url_for('users.register', username=username, email=email, password=password))
+    return render_template('facebook.html', title='Facebook', registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow)
 
 @main.route("/youtube", methods=['GET', 'POST'])
 def youtube():
-	try:
-		loginmodalshow='close'
-		loginform = LoginForm()
-		if(loginform.validate_on_submit()==False and loginform.login.data):
-			loginmodalshow='loginformmodal'
-		if loginform.validate_on_submit() and loginform.login.data:
-			remember=loginform.remember.data
-			email=loginform.email.data
-			password=loginform.password.data
-			return redirect(url_for('users.login', remember=remember, email=email, password=password))
-		modalshow='close'
-		registerform = RegistrationForm()
-		if(registerform.validate_on_submit()==False and registerform.signup.data):
-			modalshow='registerformmodal'
-		if registerform.validate_on_submit() and registerform.signup.data:
-			username=registerform.username.data
-			email=registerform.email.data
-			password=registerform.password.data
-			return redirect(url_for('users.register', username=username, email=email, password=password))
-		return render_template('youtube.html', title='Youtube', registerform=registerform, modalshow=modalshow, loginform=loginform, loginmodalshow=loginmodalshow)
-	except:
-		flash('Something went Wrong. Please check weather enterd details are correct', 'warning')
-		return redirect(url_for('main.home'))
+    try:
+        loginmodalshow='close'
+        loginform = LoginForm()
+        if(loginform.validate_on_submit()==False and loginform.login.data):
+            loginmodalshow='loginformmodal'
+        if loginform.validate_on_submit() and loginform.login.data:
+            remember=loginform.remember.data
+            email=loginform.email.data
+            password=loginform.password.data
+            return redirect(url_for('users.login', remember=remember, email=email, password=password))
+        
+        modalshow='close'
+        registerform = RegistrationForm()
+        if(registerform.validate_on_submit()==False and registerform.signup.data):
+            modalshow='registerformmodal'
+        if registerform.validate_on_submit() and registerform.signup.data:
+            username=registerform.username.data
+            email=registerform.email.data
+            password=registerform.password.data
+            return redirect(url_for('users.register', username=username, email=email, password=password))
+        
+        channeldetailmodal='close'
+        channeldetailform = ChannelDetailForm()
+        if(channeldetailform.validate_on_submit()==False and channeldetailform.search.data):
+            channeldetailmodal='channeldetails'
+        if channeldetailform.validate_on_submit() and channeldetailform.search.data:
+            channel_id = channeldetailform.name.data
+            return redirect(url_for('youtube.viewChannelDetails', name=channel_id))
+             
+        
+        sentimentanalysismodal='close'
+        sentimentanalysisform = SentimentAnalysisForm()
+        if(sentimentanalysisform.validate_on_submit()==False and sentimentanalysisform.searchSent.data):
+            sentimentanalysismodal='analysecomments'
+        if sentimentanalysisform.validate_on_submit() and sentimentanalysisform.searchSent.data:
+            video_id = sentimentanalysisform.name.data
+            return redirect(url_for('youtube.analyseComments', name=video_id))
+        
+        viewpredictionmodal='close'
+        viewpredictionform = ViewPredictionForm()
+        if(viewpredictionform.validate_on_submit()==False and viewpredictionform.viewspredict.data):
+            viewpredictionmodal='predictviews'
+        if viewpredictionform.validate_on_submit() and viewpredictionform.viewspredict.data:
+            channel_id = viewpredictionform.channel.data
+            category_id = viewpredictionform.category.data
+            dur = viewpredictionform.length.data
+            return redirect(url_for('youtube.predictViews', name=channel_id,dur=dur,category_id=category_id))
+      
+        
+        viralvideomodal = 'close'
+        viralvideofrom=ViralVideoForm()
+        if(viralvideofrom.validate_on_submit()==False):
+            viralvideomodal='viralvideos'
+        if viralvideofrom.validate_on_submit():
+            return redirect(url_for('youtube.viralVideos'))
+        
+        
+        return render_template('youtube.html', title='Youtube',viewpredictionform= viewpredictionform,viewpredictionmodal = viewpredictionmodal,
+                               sentimentanalysisform=sentimentanalysisform, registerform=registerform, channeldetailform=channeldetailform,
+                               sentimentanalysismodal=sentimentanalysismodal,channeldetailmodal = channeldetailmodal, modalshow=modalshow, loginform=loginform, 
+                               loginmodalshow=loginmodalshow,viralvideomodal=viralvideomodal,viralvideofrom=viralvideofrom)
+    except:
+        flash('Something went Wrong. Please check weather enterd details are correct', 'warning')
+        return redirect(url_for('main.home'))
 
 @main.route("/stack_overflow", methods=['GET', 'POST'])
 def stack_overflow():
