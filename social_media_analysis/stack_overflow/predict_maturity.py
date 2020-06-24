136b5ce08d3e74dcfa68a19de36583854c3de0e8
Sanju Anupa <sjanupa.17@cse.mrt.ac.lk>
2020-06-24 23:41:40 +0530

    Complete stack overflow part

--- social_media_analysis/stack_overflow/predict_maturity.py
+++ social_media_analysis/stack_overflow/predict_maturity.py
@@ -20,12 +20,16 @@ class PredictMaturity:
         self.user_id = user_id   
         
     def get_user_maturity(self):
-        self.all_questions = self.get_all_questions(self.user_id)
-        predict_data = self.convert_data_to_predictable(self.all_questions)
-        predicted_data = self.get_predict_data(predict_data)
-        self.all_questions['percentage'] = predicted_data['percentage']
-        median_number = predicted_data['percentage'].median()
-        median_number = round(median_number,1)
+        try:
+            self.all_questions = self.get_all_questions(self.user_id)
+            predict_data = self.convert_data_to_predictable(self.all_questions)
+            predicted_data = self.get_predict_data(predict_data)
+            self.all_questions['percentage'] = predicted_data['percentage']
+            median_number = predicted_data['percentage'].median()
+            median_number = round(median_number,1)
+        except KeyError:
+            median_number=0
+            self.all_questions=[]
         return median_number
 
     def get_all_questions(self,user):
@@ -44,26 +48,29 @@ class PredictMaturity:
                     break
             else:
                 break
-
             page+=1
+
+        #print(all_questions['closed_reason'])
         if not all_questions:
             return all_questions
         all_data = json_normalize(all_questions)
         all_data["creation_date"] = pd.to_datetime(all_data['creation_date'], unit='s').dt.strftime('%Y-%m-%d')
-            
+        # print(all_data["creation_date"])    
+        # all_data = all_data.sort_values(by='creation_date',ascending=True)
+        # print(all_data["creation_date"])
         return all_data
     
     def convert_data_to_predictable(self,df_nor):
         #Create a pandas data frame from normalized json data and sort its values according to 'score', 'answer_count',and 'view_count'
         newdf=pd.DataFrame(df_nor)
-    
+        newdf.reset_index(drop=True)
         #remove null values and values in 'last_edit_date' column and replays it with 0 and 1
         change_last_edit=(newdf['last_edit_date'].notnull()).astype('int')
         newdf['last_edit_date']=change_last_edit
     
         #turn 'is_answered' values in to binary 
         newdf['is_answered']=newdf['is_answered'].astype(int)
-    
+        
         #Create new column 'is_closed' and check the whether the question is closed or not.
         newdf.loc[newdf['closed_reason'].isnull(), 'is_closed'] = 1
         newdf.loc[newdf['closed_reason'].notnull(), 'is_closed'] = 0
@@ -87,139 +94,146 @@ class PredictMaturity:
         return predict_data
 
     def get_percentage_list(self):
-        data = self.all_questions[["creation_date","percentage"]]
-        data_points = []
-        data=data.sort_values(by="creation_date")
-        for i in range(len(data)) : 
-            date = data.loc[i, "creation_date"]
-            number = round(data.loc[i, "percentage"],1)
-            record = {
-                        "x" : date,
-                        "y" : number
-                     }
-            data_points.append(record)
-        data_points=json.dumps(data_points)  
-        return data_points
+        if(not self.all_questions.empty):
+            data = self.all_questions[["creation_date","percentage"]]
+            data_points = []
+            data=data.sort_values(by="creation_date",ascending=True)
+            data = data.reset_index(drop=True)
+            for i in range(len(data)) : 
+                date = data.loc[i, "creation_date"]
+                number = round(data.loc[i, "percentage"],1)
+                record = {
+                            "x" : date,
+                            "y" : number
+                        }
+                data_points.append(record)
+            data_points=json.dumps(data_points) 
+            return data_points
+        else:
+            return []
 
     def get_user_technologies(self):
-        required_data = self.all_questions["tags"]
-        languages = ["JavaScript","HTML","CSS","SQL","Python","Java","C#","PHP","C++","TypeScript","C","Ruby","Go","Assembly","Swift","Kotlin","R","VBA","Objective-C","Scala","Rust","Dart","Elixir","Clojure","F#","Erlang"]
-        web_frameworks = ["jQuery","Reactjs","Angular","ASP.NET","Express","Spring","Vuejs","Django","Flask","Laravel","Ruby","Ruby-on-Rails","Drupal","Meteor","Ember.js"]
-        other_frameworks = ["Nodejs",".NET",".Net-Core","Pandas","Unity3D","React-Native","Tenserflow","Ansible","Cordova","Xamarin","Apache-Spark","Hadoop","Unreal-Engine","Flutter","PyTorch","Puppet","Chef","CryEngine","SciPy","Numpy","Keras","Scikit-Learn","Eli5"]
-        databases = ["MySQL","PostgreSQL","SQL-Server","SQLite","MongoDB","Redis","MariaDB","Oracle","ElasticSearch","Firebase","Amazon-DynamoDB","Cassandra","Couchbase"]
-        platforms = ["Linux","Windows","Docker","Android","AWS","MacOS","Slack","Raspberry-Pi","WordPress","IOS","Google-Cloud-Platform","Azure","Arduino","Heroku","Kubernetes","IBM-Cloud"]
+        if(not self.all_questions.empty):
+            required_data = self.all_questions["tags"]
+            languages = ["JavaScript","HTML","CSS","SQL","Python","Java","C#","PHP","C++","TypeScript","C","Ruby","Go","Assembly","Swift","Kotlin","R","VBA","Objective-C","Scala","Rust","Dart","Elixir","Clojure","F#","Erlang"]
+            web_frameworks = ["jQuery","Reactjs","Angular","ASP.NET","Express","Spring","Vuejs","Django","Flask","Laravel","Ruby","Ruby-on-Rails","Drupal","Meteor","Ember.js"]
+            other_frameworks = ["Nodejs",".NET",".Net-Core","Pandas","Unity3D","React-Native","Tenserflow","Ansible","Cordova","Xamarin","Apache-Spark","Hadoop","Unreal-Engine","Flutter","PyTorch","Puppet","Chef","CryEngine","SciPy","Numpy","Keras","Scikit-Learn","Eli5"]
+            databases = ["MySQL","PostgreSQL","SQL-Server","SQLite","MongoDB","Redis","MariaDB","Oracle","ElasticSearch","Firebase","Amazon-DynamoDB","Cassandra","Couchbase"]
+            platforms = ["Linux","Windows","Docker","Android","AWS","MacOS","Slack","Raspberry-Pi","WordPress","IOS","Google-Cloud-Platform","Azure","Arduino","Heroku","Kubernetes","IBM-Cloud"]
 
-        lower_case_data = []
+            lower_case_data = []
 
-        languages_low = [i.lower() for i in languages]
-        web_frameworks_low = [i.lower() for i in web_frameworks]
-        other_frameworks_low = [i.lower() for i in other_frameworks]
-        databases_low = [i.lower() for i in databases]
-        platforms_low = [i.lower() for i in platforms]
-        
-        lower_case_data.extend(languages_low)
-        lower_case_data.extend(web_frameworks_low)
-        lower_case_data.extend(other_frameworks_low)
-        lower_case_data.extend(databases_low)
-        lower_case_data.extend(platforms_low)
-        lower_case_data.extend(["python-3.x","python-2.7"])
-        
-        user_tecs = []
-        temp_tags = []
-        for tags in required_data:
-            #cd stackprint(tags)
-            '''tags = tags.replace("[","")
-            tags = tags.replace("]","")
-            tags = tags.replace("'","")
-            tags = tags.split(",")'''
-            tempTag = []
-            for i in tags:
-                temp = i.strip()
-                if temp in lower_case_data:
-                    if temp == "python" or temp == "python-3.x" or temp == "python-2.7":
-                        if "python" not in tempTag:
-                            tempTag.append("python")
-                        if "python" not in user_tecs:
-                            user_tecs.append("python")
-                    else:
-                        if temp not in tempTag:
-                            tempTag.append(temp)
-                        if temp not in user_tecs:
-                            user_tecs.append(temp)
-            temp_tags.append(tempTag)
-        
-        data = {}
-        data["languages"] = []
-        data["web_frameworks"] = []
-        data["other_frameworks"] = []
-        data["databases"] = []
-        data["platforms"] = []
-        #print(data)
-        for tech in user_tecs:
-            count = 0
-            for techs in temp_tags:
-                if tech in techs:
-                    count+=1
-            percentage = count*100/len(temp_tags)
-            percentage = round(percentage,1)
-            name = ""
-            if tech in languages_low:
-                name = languages[languages_low.index(tech)]
-                record = {
-                            "name" : name,
-                            "percentage" : percentage
-                    
-                        }
-                data["languages"].append(record)  
-            elif tech in web_frameworks_low:
-                name = web_frameworks[web_frameworks_low.index(tech)]
-                record = {
-                            "name" : name,
-                            "percentage" : percentage
-                    
-                        }
-                data["web_frameworks"].append(record) 
-                
-            elif tech in other_frameworks_low:
-                name = other_frameworks[other_frameworks_low.index(tech)]
-                record = {
-                            "name" : name,
-                            "percentage" : percentage
-                    
-                        }
-                data["other_frameworks"].append(record) 
-                
-            elif tech in databases_low:
-                name = databases[databases_low.index(tech)]
-                record = {
-                            "name" : name,
-                            "percentage" : percentage
-                    
-                        }
-                data["databases"].append(record) 
-                
-            elif tech in platforms_low:
-                name = platforms[platforms_low.index(tech)]
-                record = {
-                            "name" : name,
-                            "percentage" : percentage
-                    
-                        }
-                data["platforms"].append(record) 
-        if not data["languages"] :
-            data["languages"] = 0
-        if not data["web_frameworks"] :
-            data["web_frameworks"] = 0
-        if not data["other_frameworks"] :
-            data["other_frameworks"] = 0
-        if not data["databases"] :
-            data["databases"] = 0
-        if not data["platforms"] :
-            data["platforms"] = 0
-        
+            languages_low = [i.lower() for i in languages]
+            web_frameworks_low = [i.lower() for i in web_frameworks]
+            other_frameworks_low = [i.lower() for i in other_frameworks]
+            databases_low = [i.lower() for i in databases]
+            platforms_low = [i.lower() for i in platforms]
             
-        data=json.dumps(data)
-        return data
+            lower_case_data.extend(languages_low)
+            lower_case_data.extend(web_frameworks_low)
+            lower_case_data.extend(other_frameworks_low)
+            lower_case_data.extend(databases_low)
+            lower_case_data.extend(platforms_low)
+            lower_case_data.extend(["python-3.x","python-2.7"])
+            
+            user_tecs = []
+            temp_tags = []
+            for tags in required_data:
+                #cd stackprint(tags)
+                '''tags = tags.replace("[","")
+                tags = tags.replace("]","")
+                tags = tags.replace("'","")
+                tags = tags.split(",")'''
+                tempTag = []
+                for i in tags:
+                    temp = i.strip()
+                    if temp in lower_case_data:
+                        if temp == "python" or temp == "python-3.x" or temp == "python-2.7":
+                            if "python" not in tempTag:
+                                tempTag.append("python")
+                            if "python" not in user_tecs:
+                                user_tecs.append("python")
+                        else:
+                            if temp not in tempTag:
+                                tempTag.append(temp)
+                            if temp not in user_tecs:
+                                user_tecs.append(temp)
+                temp_tags.append(tempTag)
+            
+            data = {}
+            data["languages"] = []
+            data["web_frameworks"] = []
+            data["other_frameworks"] = []
+            data["databases"] = []
+            data["platforms"] = []
+            #print(data)
+            for tech in user_tecs:
+                count = 0
+                for techs in temp_tags:
+                    if tech in techs:
+                        count+=1
+                percentage = count*100/len(temp_tags)
+                percentage = round(percentage,1)
+                name = ""
+                if tech in languages_low:
+                    name = languages[languages_low.index(tech)]
+                    record = {
+                                "name" : name,
+                                "percentage" : percentage
+                        
+                            }
+                    data["languages"].append(record)  
+                elif tech in web_frameworks_low:
+                    name = web_frameworks[web_frameworks_low.index(tech)]
+                    record = {
+                                "name" : name,
+                                "percentage" : percentage
+                        
+                            }
+                    data["web_frameworks"].append(record) 
+                    
+                elif tech in other_frameworks_low:
+                    name = other_frameworks[other_frameworks_low.index(tech)]
+                    record = {
+                                "name" : name,
+                                "percentage" : percentage
+                        
+                            }
+                    data["other_frameworks"].append(record) 
+                    
+                elif tech in databases_low:
+                    name = databases[databases_low.index(tech)]
+                    record = {
+                                "name" : name,
+                                "percentage" : percentage
+                        
+                            }
+                    data["databases"].append(record) 
+                    
+                elif tech in platforms_low:
+                    name = platforms[platforms_low.index(tech)]
+                    record = {
+                                "name" : name,
+                                "percentage" : percentage
+                        
+                            }
+                    data["platforms"].append(record) 
+            if not data["languages"] :
+                data["languages"] = 0
+            if not data["web_frameworks"] :
+                data["web_frameworks"] = 0
+            if not data["other_frameworks"] :
+                data["other_frameworks"] = 0
+            if not data["databases"] :
+                data["databases"] = 0
+            if not data["platforms"] :
+                data["platforms"] = 0
+            
+                
+            data=json.dumps(data)
+            return data
+        else:
+            return []
 
 
 
@@ -227,17 +241,17 @@ class PredictMaturity:
 
 
 
-# c = PredictMaturity(20654)   
+c = PredictMaturity(20654)   
 
-# print (c.get_user_maturity())
+print (c.get_user_maturity())
 # print(c.get_percentage_list())
-# print(c.get_user_technologies())
+# print(c.get_user_technologies()) 
 
 
 
 '''cwd = os.getcwd()  # Get the current working directory (cwd)
 files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))'''
+print("Files in %r: %s" % (cwd, files)) '''
 
 # In[ ]:
 
--- social_media_analysis/stack_overflow/routes.py
+++ social_media_analysis/stack_overflow/routes.py
@@ -5,33 +5,23 @@ from social_media_analysis.models import User, Post
 from social_media_analysis.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
                                    RequestResetForm, ResetPasswordForm)
 from social_media_analysis.stack_overflow import predict_maturity
-
+import json
 stack_overflow = Blueprint('stack_overflow', __name__)
 
 @stack_overflow.route("/maturity", methods=["POST"])
 def maturity():    
     data=request.form["userID"]
-    dates=[
-            {
-                "x":"2017-03-02",
-                "y":45
-            },
-            {
-                "x":"2017-04-02",
-                "y":25
-            },
-            {
-                "x":"2017-06-02",
-                "y":73
-            }
-
-        ]
-    barchartData={"languages": [{"name": "Java", "percentage": 37.0}, {"name": "Python", "percentage": 6.2}, {"name": "Ruby", "percentage": 3.3}, {"name": "HTML", "percentage": 2.2}, {"name": "CSS", "percentage": 0.7}, {"name": "Go", "percentage": 4.7}, {"name": "JavaScript", "percentage": 12.0}, {"name": "C#", "percentage": 7.2}, {"name": "C++", "percentage": 1.1}, {"name": "C", "percentage": 0.4}, {"name": "SQL", "percentage": 5.1}, {"name": "Scala", "percentage": 4.0}, {"name": "Clojure", "percentage": 0.4}, {"name": "Objective-C", "percentage": 0.4}], "web_frameworks": [{"name": "Reactjs", "percentage": 0.4}, {"name": "Express", "percentage": 0.4}, {"name": "Spring", "percentage": 1.1}], "other_frameworks": 0, "databases": [{"name": "Oracle", "percentage": 4.3}, {"name": "PostgreSQL", "percentage": 0.4}, {"name": "SQL-Server", "percentage": 0.7}], "platforms": [{"name": "MacOS", "percentage": 2.5}, {"name": "Windows", "percentage": 0.7}, {"name": "Linux", "percentage": 0.4}]}
-    id=46
+    dates=[] 
+    barchartData={"languages": [ {"name": "Python", "percentage": 6.2}, {"name": "Ruby", "percentage": 3.3}, {"name": "HTML", "percentage": 2.2}, {"name": "CSS", "percentage": 0.7}, {"name": "Go", "percentage": 4.7}, {"name": "JavaScript", "percentage": 12.0}, {"name": "C#", "percentage": 7.2}, {"name": "C++", "percentage": 1.1}, {"name": "C", "percentage": 0.4}, {"name": "SQL", "percentage": 5.1}, {"name": "Scala", "percentage": 4.0}, {"name": "Clojure", "percentage": 0.4}, {"name": "Objective-C", "percentage": 0.4}], "web_frameworks": [{"name": "Reactjs", "percentage": 0.4}, {"name": "Express", "percentage": 0.4}, {"name": "Spring", "percentage": 1.1}], "other_frameworks": 0, "databases": [{"name": "Oracle", "percentage": 4.3}, {"name": "PostgreSQL", "percentage": 0.4}, {"name": "SQL-Server", "percentage": 0.7}], "platforms": [{"name": "MacOS", "percentage": 2.5}, {"name": "Windows", "percentage": 0.7}, {"name": "Linux", "percentage": 0.4}]}
+    
     mat = predict_maturity.PredictMaturity(data)
     percentage = mat.get_user_maturity()
     dates = mat.get_percentage_list()
     barchartData=mat.get_user_technologies()
-    print(dates)
-    print(barchartData)
+    dates=json.loads(dates)
+    barchartData=json.loads(barchartData)
+    
+
     return jsonify({"id":percentage,"dates":dates,"barchartData":barchartData})
+
+
