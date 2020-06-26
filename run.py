1290184ec5f750c76b9a8678bbfbe1f71d445d5b
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2020-06-27 00:25:46 +0530

    secret key removed fb

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from social_media_analysis import create_app
 
 app = create_app()
-import os
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+
  
 if __name__ == '__main__':
     app.run(debug=True)
