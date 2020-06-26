7d5a0ade46444d309950d0969af1de61006c1861
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-06-26 19:18:50 +0530

    Update config.py

--- social_media_analysis/config.py
+++ social_media_analysis/config.py
@@ -6,8 +6,12 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     #SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/social_media_analysis'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 443
-    MAIL_USE_TLS = True
+    #MAIL_SERVER = 'smtp.googlemail.com'
+    #MAIL_PORT = 25
+    #MAIL_USE_TLS = True
+    MAIL_SERVER: 'smtp.gmail.com',
+    MAIL_PORT: 465,
+    MAIL_USE_TLS: False,
+    MAIL_USE_SSL: True,
     MAIL_USERNAME = 'smanalysis.uom@gmail.com'
     MAIL_PASSWORD = 'Smanalysis@123'
