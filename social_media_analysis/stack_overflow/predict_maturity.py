f3448d4c4d14c839b1b9124f2acff093b174f405
Sanju Anupa <sjanupa.17@cse.mrt.ac.lk>
2020-06-29 19:07:35 +0530

    Frontend edited with predict maturity

--- social_media_analysis/stack_overflow/predict_maturity.py
+++ social_media_analysis/stack_overflow/predict_maturity.py
@@ -94,7 +94,7 @@ class PredictMaturity:
         return predict_data
 
     def get_percentage_list(self):
-        if(not self.all_questions.empty):
+        if( type(self.all_questions)!=list and not self.all_questions.empty):
             data = self.all_questions[["creation_date","percentage"]]
             data_points = []
             data=data.sort_values(by="creation_date",ascending=True)
@@ -110,10 +110,12 @@ class PredictMaturity:
             data_points=json.dumps(data_points) 
             return data_points
         else:
-            return []
+            data=[]
+            data=json.dumps([])
+            return data
 
     def get_user_technologies(self):
-        if(not self.all_questions.empty):
+        if(type(self.all_questions)!=list and not self.all_questions.empty):
             required_data = self.all_questions["tags"]
             languages = ["JavaScript","HTML","CSS","SQL","Python","Java","C#","PHP","C++","TypeScript","C","Ruby","Go","Assembly","Swift","Kotlin","R","VBA","Objective-C","Scala","Rust","Dart","Elixir","Clojure","F#","Erlang"]
             web_frameworks = ["jQuery","Reactjs","Angular","ASP.NET","Express","Spring","Vuejs","Django","Flask","Laravel","Ruby","Ruby-on-Rails","Drupal","Meteor","Ember.js"]
@@ -233,7 +235,9 @@ class PredictMaturity:
             data=json.dumps(data)
             return data
         else:
-            return []
+            data=[]
+            data=json.dumps([])
+            return data
 
 
 
